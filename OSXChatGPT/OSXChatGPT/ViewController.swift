8269bff368fbbad44beac18e7213d0a5a147513f
MustangYM <MustangYM@yeah.net>
2023-03-05 13:07:21 +0800

    init commit

---
+++ OSXChatGPT/OSXChatGPT/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  OSXChatGPT
+//
+//  Created by MustangYM on 2023/3/5.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
