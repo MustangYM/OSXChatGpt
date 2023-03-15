6ac502b3c9da8997ab45bf53b2464fc4c4783a3d
MustangYM <MustangYM@yeah.net>
2023-03-15 12:30:59 +0800

    Fix the thing animation

--- OSXChatGPT/OSXChatGPT/WindowView/ThinkingAnimationView.swift
+++ OSXChatGPT/OSXChatGPT/WindowView/ThinkingAnimationView.swift
@@ -10,15 +10,13 @@ import SwiftUI
 struct ThinkingAnimationView: View {
     var body: some View {
         HStack(spacing: 6) {
-            ThinkingAnimationFirstCircle(color: .black)
-                .padding(.trailing, 10)
-                .padding(.leading, 12)
-            ThinkingAnimationCircle(delay: 0.0, color: .red)
-            ThinkingAnimationCircle(delay: 0.2, color: .orange)
-            ThinkingAnimationCircle(delay: 0.4, color: .yellow)
-            ThinkingAnimationCircle(delay: 0.6, color: .green)
-            ThinkingAnimationCircle(delay: 0.8, color: .blue)
-            ThinkingAnimationCircle(delay: 1.0, color: .purple)
+            ThinkingAnimationFirstCircle(color: NSColor(r: 224, g: 87, b: 114).toColor())
+            ThinkingAnimationCircle(delay: 0.0, color: NSColor(r: 224, g: 87, b: 114).toColor())
+            ThinkingAnimationCircle(delay: 0.2, color: NSColor(r: 196, g: 107, b: 145).toColor())
+            ThinkingAnimationCircle(delay: 0.4, color: NSColor(r: 171, g: 118, b: 170).toColor())
+            ThinkingAnimationCircle(delay: 0.6, color: NSColor(r: 131, g: 127, b: 205).toColor())
+            ThinkingAnimationCircle(delay: 0.8, color: NSColor(r: 128, g: 109, b: 221).toColor())
+            ThinkingAnimationCircle(delay: 1.0, color: NSColor(r: 80, g: 130, b: 238).toColor())
             Spacer()
         }
         .frame(width: 105, height: 15)
