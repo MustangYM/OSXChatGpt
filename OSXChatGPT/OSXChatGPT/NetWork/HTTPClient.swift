6328aa1bdcc7f8faa62ae50f56339bfd11b7377f
陈连辰 <lianchen551@163.com>
2023-03-25 20:15:37 +0800

    input stream

--- OSXChatGPT/OSXChatGPT/NetWork/HTTPClient.swift
+++ OSXChatGPT/OSXChatGPT/NetWork/HTTPClient.swift
@@ -7,10 +7,49 @@
 
 import Foundation
 
+enum HTTPError: Error {
+    case err(message: String)
+    var message: String {
+        switch self {
+        case .err(let er):
+            return er
+        }
+    }
+}
+struct HTTPResponseError: Decodable {
+    let error: HTTPResponseErrorEntity
+}
+
+struct HTTPResponseErrorEntity: Decodable {
+    let message: String
+    let type: String?
+}
+struct HTTPResponse: Decodable {
+    let choices: [HTTPResponseChoice]
+    let model: String
+}
+
+struct HTTPResponseChoice: Decodable {
+    let finishReason: String?
+    let delta: HTTPResponseMessage?
+    let message: HTTPResponseMessage?
+}
+
+struct HTTPResponseMessage: Decodable {
+    let content: String?
+    let role: String?
+}
+
+
 class HTTPClient {
 //    static let shared = HTTPClient()
     fileprivate var urlSession: URLSession!
     fileprivate var sessionConfiguration: URLSessionConfiguration!
+    private let jsonDecoder: JSONDecoder = {
+        let jsonDecoder = JSONDecoder()
+        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        return jsonDecoder
+    }()
     init() {
         sessionConfiguration = URLSessionConfiguration.default
         urlSession = URLSession(configuration: sessionConfiguration)
@@ -19,19 +58,9 @@ class HTTPClient {
     func setAdditionalHeaders(_ headers: Dictionary<String, AnyObject>) {
         sessionConfiguration.httpAdditionalHeaders = headers
     }
-
+    
     func post(url: URL, headers: [String: String], parameters: [String: Any], callback:@escaping (_ data: Data?, _ error: NSError?, _ resCode: Int?) -> Void) {
-        var request = URLRequest(url: url)
-        request.allHTTPHeaderFields = headers
-        guard let postData = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {
-            let statusError = NSError(domain:"Request parameters Error", code:-1, userInfo:[NSLocalizedDescriptionKey: "请求参数错误"])
-            DispatchQueue.main.async {
-                callback(nil, statusError, -1)
-            }
-            return
-        }
-        request.httpMethod = "POST"
-        request.httpBody = postData
+        let request = createRequest(url: url, headers: headers, parameters: parameters)
         let task = urlSession.dataTask(with: request) { data, response, error in
             if let responseError = error {
                 DispatchQueue.main.async {
@@ -60,6 +89,62 @@ class HTTPClient {
             }
         }
         task.resume()
+    }
+    
+    func postStream(chatRequest: ChatGPTRequest) async throws -> AsyncThrowingStream<String, Error> {
+        var request = URLRequest(url: chatRequest.url)
+        request.allHTTPHeaderFields = chatRequest.headers
+        if let postData = try? JSONSerialization.data(withJSONObject: chatRequest.parameters, options: []) {
+            request.httpBody = postData
+        }
+        request.httpMethod = "POST"
+        let (result, response) = try await urlSession.bytes(for: request)
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw HTTPError.err(message: "Request Error")
+        }
+        guard 200...299 ~= httpResponse.statusCode else {
+            var errorText = ""
+            for try await line in result.lines {
+                errorText += line
+            }
+            if let data = errorText.data(using: .utf8), let response = try? jsonDecoder.decode(HTTPResponseError.self, from: data).error {
+                errorText = "\n\(response.message)"
+            }
+            throw HTTPError.err(message: "Response Error code:\(httpResponse.statusCode) res:\(errorText)")
+        }
+        return AsyncThrowingStream<String, Error> { continuation in
+            Task(priority: .userInitiated) { [weak self] in
+                do {
+                    for try await line in result.lines {
+                        if line.hasPrefix("data: "),
+                           let data = line.dropFirst(6).data(using: .utf8),
+                           let response = try? self?.jsonDecoder.decode(HTTPResponse.self, from: data),
+                           let text = response.choices.first?.delta?.content
+                        {
+                            continuation.yield(text)
+                        }else if let data = line.data(using: .utf8),
+                                    let response = try? self?.jsonDecoder.decode(HTTPResponse.self, from: data),
+                                    let text = response.choices.first?.message?.content {
+                            continuation.yield(text)
+                            
+                        }
+                    }
+                    continuation.finish()
+                } catch {
+                    continuation.finish(throwing: error)
+                }
+            }
+        }
+    }
+    
+    private func createRequest(url: URL, headers: [String: String], parameters: [String: Any]) -> URLRequest {
+        var request = URLRequest(url: url)
+        request.allHTTPHeaderFields = headers
+        if let postData = try? JSONSerialization.data(withJSONObject: parameters, options: []) {
+            request.httpBody = postData
+        }
+        request.httpMethod = "POST"
+        return request
     }
 
 }
---
+++ OSXChatGPT/OSXChatGPT/WindowView/ChatRoomInputView.swift
@@ -0,0 +1,121 @@
+//
+//  ChatRoomInputView.swift
+//  OSXChatGPT
+//
+//  Created by CoderChan on 2023/3/25.
+//
+
+import SwiftUI
+
+struct ChatRoomInputView: View {
+    @EnvironmentObject var viewModel: ViewModel
+    @State private var newMessageText = ""
+    @State private var lastMessageText = ""
+    @Binding var inputViewHeight: CGFloat
+    var body: some View {
+        GeometryReader { geometry in
+            if #available(macOS 13.0, *) {
+                TextEditor(text: $newMessageText)
+                    .font(.title3)
+                    .lineSpacing(2)
+                    .disableAutocorrection(true)
+                    .padding()
+                    .background(Color.clear)
+                //                            .scrollContentBackground(.hidden)
+                    .cornerRadius(10)
+                    .frame(maxHeight: geometry.size.height)
+                    .onChange(of: newMessageText) { newValue in
+                        onTextViewChange(newValue)
+                    }
+                
+            } else {
+                TextEditor(text: $newMessageText)
+                    .font(.title3)
+                    .lineSpacing(2)
+                    .disableAutocorrection(true)
+                    .padding()
+                    .background(Color.clear)
+                    .cornerRadius(10)
+                    .frame(maxHeight: geometry.size.height)
+                    .onChange(of: newMessageText) { newValue in
+                        onTextViewChange(newValue)
+                    }
+                
+            }
+        }
+        .padding(0)
+        .frame(height: inputViewHeight)
+    }
+    
+    
+    private func onTextViewChange(_ newValue: String) {
+        
+        if (KeyboardMonitor.shared.shiftKeyPressed) {
+            //按下shift
+        }else if newMessageText.count < lastMessageText.count {
+            //删除操作
+        }else if newMessageText.count - 1 == lastMessageText.count {
+            //在中间任意地方按下空格键发送
+            let charts = compareText(newText: newMessageText, lastText: lastMessageText)
+            if charts == "\n" {
+                if KeyboardMonitor.shared.currentPasteboardText == newMessageText {
+                    //复制进来的，不发送
+                }else {
+                    //输入的是空格，则发送
+                    sendMessage(scrollView: nil, text: lastMessageText)
+                }
+            }
+        }
+        lastMessageText = newMessageText
+    }
+    
+    private func sendMessage(scrollView: ScrollViewProxy?, text: String) {
+        guard !newMessageText.isEmpty else { return }
+        let msg = String(newMessageText.dropLast())
+        let replaceStr = msg.replacingOccurrences(of: " ", with: "")
+        if replaceStr.count == 0 {
+            cleanText()
+            return
+        }else if replaceStr.contains("\n") {
+            let repl = replaceStr.replacingOccurrences(of: "\n", with: "")
+            if repl.count == 0 {
+                cleanText()
+                return
+            }
+        }
+        viewModel.addNewMessage(sesstionId: viewModel.currentConversation?.sesstionId ?? "", text: msg, role: "user") {
+            
+        }
+        //清空
+        cleanText()
+    }
+    
+    private func cleanText() {
+        newMessageText = ""
+//        conversation?.lastInputText = ""
+        lastMessageText = ""
+    }
+    
+    private func compareText(newText: String, lastText: String) -> String {
+        var newValue = String(newText)
+        var lastValue = String(lastText)
+        //比较两个字符串，找出新输入的那个字符
+        newText.forEach { chart in
+            if let index = newValue.firstIndex(of: chart) {
+                if let idx = lastValue.firstIndex(of: chart) {
+                    newValue.remove(at: index)
+                    lastValue.remove(at: idx)
+                }
+            }
+        }
+        return newValue
+    }
+    
+}
+
+//struct ChatRoomInputView_Previews: PreviewProvider {
+//    @State private var inputViewSize = CGSize(width: 200, height: 200)
+//    static var previews: some View {
+//        ChatRoomInputView(size: $inputViewSize)
+//    }
+//}
